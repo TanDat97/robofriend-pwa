4c584e8bed0a66d51ceab3363dc31ddf9a6d93bf
Tran Tan Dat <trantandat130497@gmail.com>
2020-06-29 21:23:16 +0700

    Updates

---
+++ precache-manifest.f51bd1503b3472163907fc68125b7d66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430f3a558a3b6208e0945550f246d887",
+    "url": "/robofriends-pwa/index.html"
+  },
+  {
+    "revision": "5b833e80a684fa1ea596",
+    "url": "/robofriends-pwa/static/css/2.7a3f122f.chunk.css"
+  },
+  {
+    "revision": "57cea8b23059d6b9c948",
+    "url": "/robofriends-pwa/static/css/main.84315f23.chunk.css"
+  },
+  {
+    "revision": "5b833e80a684fa1ea596",
+    "url": "/robofriends-pwa/static/js/2.ad415742.chunk.js"
+  },
+  {
+    "revision": "57cea8b23059d6b9c948",
+    "url": "/robofriends-pwa/static/js/main.80ede5fa.chunk.js"
+  },
+  {
+    "revision": "3f34f6f567ce15bf82ba",
+    "url": "/robofriends-pwa/static/js/runtime-main.628adadd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-pwa/static/media/SEGA.12a3db8b.woff"
+  }
+]);
